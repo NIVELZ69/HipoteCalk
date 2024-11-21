8c7421273c0dca752371f4919dab3fad6c3415a7
NIVELZ69 <blackwolfshadowkiller@gmail.com>
2024-11-21 12:10:13 +0000

    Añadido proyecto y FXML

---
+++ src/main/java/dad/HipoteCalk/HipotecApp.java
@@ -0,0 +1,14 @@
+package dad.HipoteCalk;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class HipotecApp extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/dad/HipoteCalk/Main.java
@@ -0,0 +1,10 @@
+package dad.HipoteCalk;
+
+public class Main {
+
+	public static void main(String[] args) {
+				
+
+	}
+
+}
